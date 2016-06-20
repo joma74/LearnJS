e9de380bca7cdfd6c45677ece70f983a595d5fb4
joma74 <dev-mails@gmx.net>
2016-06-21 01:41:28 +0200

    Added gulp and npm workflow for js building

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,4 +1,17 @@
-import gulp from "gulp";
-import browserify from "browserify";
-import source from "vinyl-source-stream"
+var gulp = require('gulp');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
 
+gulp.task("transpile", () => {
+    return browserify('src/forumapp/app.js')
+        .transform('babelify')
+        .bundle()
+        .pipe(source('forumapp-bundle.js'))
+        .pipe(gulp.dest('target'));
+});
+
+gulp.task("watch", () => {
+   gulp.watch('src/forumapp/**/*.js', ['transpile'])
+});
+
+gulp.task("default", ["transpile"]);
