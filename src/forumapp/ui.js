6a990554163cc4c0139ab0c7a2019e7128e4c225
joma74 <dev-mails@gmx.net>
2016-06-21 22:17:06 +0200

    with xss filter on data rendering

--- src/forumapp/ui.js
+++ src/forumapp/ui.js
@@ -1,3 +1,5 @@
+import xss from "xss-filters";
+
 let ui = {
     renderPosts(posts){
         console.log(posts);
@@ -12,13 +14,15 @@ let ui = {
 };
 
 function articleTemplate(title, lastReply) {
+    let safeTitle = xss.inHTMLData(title);
+    let safeLastReply = xss.inHTMLData(lastReply);
     let template = `
         <article class="post">
             <h2 class="post-title">
-                ${title}
+                ${safeTitle}
             </h2>
         <p class="post-meta">
-            ${lastReply}
+            ${safeLastReply}
         </p>
         </article>
     `;
