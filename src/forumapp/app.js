54c99ac73e631bcf2f004bc34851c13ade7c284d
joma74 <dev-mails@gmx.net>
2016-06-21 17:57:39 +0200

    with json-server integration

--- src/forumapp/app.js
+++ src/forumapp/app.js
@@ -1,7 +1,20 @@
 let Post = {
     findAll(){
         return new Promise((resolve, reject) => {
-            resolve("ok posts!");
+            let uri = "http://localhost:3000/posts";
+            let request = new XMLHttpRequest();
+            request.open("GET", uri, true);
+            request.onload = () => {
+                if (request.status >= 200 && request.status < 400) {
+                    resolve(JSON.parse(request.response));
+                }
+            };
+
+            request.onerror = () => {
+                reject(new Error("Something went wrong on the API"));
+            };
+
+            request.send();
         });
     }
 }
@@ -11,4 +24,4 @@ let ui = {
         console.log(posts);
     }
 }
-Post.findAll().then(ui.renderPosts);
+Post.findAll().then(ui.renderPosts).catch((error) => console.log(error));
