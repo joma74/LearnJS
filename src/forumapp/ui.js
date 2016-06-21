e0860f154493bad14f9985df145ba97304a7007b
joma74 <dev-mails@gmx.net>
2016-06-21 21:34:27 +0200

    with server integration and rendering

--- src/forumapp/ui.js
+++ src/forumapp/ui.js
@@ -1,7 +1,29 @@
 let ui = {
     renderPosts(posts){
         console.log(posts);
+        let postElements = posts.map((post) => {
+            let {title, lastReply} = post;
+            return articleTemplate(title, lastReply);
+        });
+
+        let target = document.querySelector(".container");
+        target.innerHTML = postElements.join("");
     }
 };
 
+function articleTemplate(title, lastReply) {
+    let template = `
+        <article class="post">
+            <h2 class="post-title">
+                ${title}
+            </h2>
+        <p class="post-meta">
+            ${lastReply}
+        </p>
+        </article>
+    `;
+    return template;
+}
+
 export default ui;
+
